d3eed072048750c8a8fe331631f00887648cfa7c
Laure <nilugette@yahoo.fr>
2021-08-25 11:52:24 +0200

    MapBox integration

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
       domains: ['links.papareact.com'],
     },
+    env: {
+      mapbox_key: "pk.eyJ1IjoibmlsdWdldHRlIiwiYSI6ImNrc3I3N2U3czBqbHozMm9kdXJyaW8wejUifQ.y8e_G2EO-GfG7itrD_KOXg"
+    }
 }
